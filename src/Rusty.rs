15f85aaddece45ba8c7aad891d21613b305c296c
Yash <yashyt6246@gmail.com>
2026-07-04 11:50:52 +0530

    Cli Edit

--- src/Rusty.rs
+++ src/Rusty.rs
@@ -1,6 +1,6 @@
 use std::io; //alternative to C++'s "#include <iostream> "
 use std::io::Write;
-fn input() -> (i64, i64) {
+fn minput() -> (i64, i64) {
     let mut num1 = String::new();
     let mut num2 = String::new();
     print!("Enter Number: ");
@@ -13,39 +13,47 @@ fn input() -> (i64, i64) {
     let num2: i64 = num2.trim().parse().expect("Enter a number!");
     (num1, num2)
 }
+fn sinput() -> i64 {
+    let mut num = String::new();
+    print!("Enter Number: ");
+    io::stdout().flush().unwrap();
+    std::io::stdin().read_line(&mut num).expect("Huh! Somethings Wroong...... with num1");
+    let num: i64 = num.trim().parse().expect("Enter a number!");
+    num
+}
 fn add() {
-    let (num1, num2) = input();
+    let (num1, num2) = minput();
     let res = num1 + num2;
     println!("Result: {}", res);
 }
 fn sub() {
-    let (num1, num2) = input();
+    let (num1, num2) = minput();
     let res = num1 - num2;
     println!("Result: {}", res);
 
 }
 fn multi() {
-    let (num1, num2) = input();
+    let (num1, num2) = minput();
     let res = num1 * num2;
     println!("Result: {}", res);
 }
 fn div() {
-    let (num1, num2) = input();
+    let (num1, num2) = minput();
     let res = num1 / num2;
     println!("Result: {}", res);
 }
 fn remain() {
-    let (num1, num2) = input();
+    let (num1, num2) = minput();
     let res = num1 % num2;
     println!("Result: {}", res);
 }
 fn prcnt() {
-    let (num1, num2) = input();
+    let (num1, num2) = minput();
     let res = (num1 as f64 / num2 as f64 ) * 100.0 ;
     println!("{num1} Is {res}% of {num2}");
 }
 fn expo() {
-    let (base, mut power) = input();
+    let (base, mut power) = minput();
     let usedpower = power;
     let mut res = 1;
     while power > 0 {res *= base;
@@ -53,6 +61,11 @@ fn expo() {
 
     };
     println!("{base} ^ {usedpower} Is {res}");
+}
+fn absval() {
+    let num = sinput();
+    let res = num.abs();
+    println!("Absolute Value: {res}");
 }
 fn main() {
     let mut operator = String::new();
@@ -88,6 +101,10 @@ fn main() {
         else if operator.trim() == "^" {
             operator.clear();
             expo();
+        }
+        else if operator.trim() == "|" {
+            operator.clear();
+            absval();
         }
         else {break;}
     }
